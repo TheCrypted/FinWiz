ab7f59997d69ea9b387d7fa0222819d516175566
Aman Sharma <mail.aman@protonmail.com>
2024-10-30 13:25:51 -0400

    auth pages + basic outline + new packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import {SignIn} from "./pages/auth/SignIn.jsx";
+import {SignUp} from "./pages/auth/SignUp.jsx";
+
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <>
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+        </>
+    ),
+    { basename: import.meta.env.BASE_URL }
+);
 
 function App() {
 
     return (
-    <>
-        <div className="w-full h-full bg-black">
-
-        </div>
-    </>
-    )
+        <RouterProvider router={router} />
+    );
 }
-
 export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {MousePositionProvider} from "./context/MousePositionProvider.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <MousePositionProvider>
+          <App />
+      </MousePositionProvider>
   </StrictMode>,
 )
